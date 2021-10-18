f1daa2d632db168d331f746f6de4fca3be03257f
Warnet <warnet1920@gmail.com>
2021-10-19 00:00:35 +0700

    add SFML and ModelView class

--- Test Environment/Test Environment/Test Environment/main.cpp
+++ Test Environment/Test Environment/Test Environment/main.cpp
@@ -1,18 +1,22 @@
 #include "Log.h"
 #include "CheckersGame.h"
+#include "ModelView.h"
 
 using namespace std;
 
 int main()
 {
-	Log::Init("log.txt");
-	CheckersGame game;
+	
+	ModelView test;
+	test.Start();
+	//Log::Init("log.txt");
+	//CheckersGame game;
 
-	game.GetChosenChecker(1, 1);
-	game.GetChosenChecker(7, 3);
-	game.GetChosenChecker(5, 5);
+	//game.GetChosenChecker(1, 1);
+	//game.GetChosenChecker(7, 3);
+	//game.GetChosenChecker(5, 5);
 
-	game.NewGame();
+	//game.NewGame();
 
-	Log::Stop();
+	//Log::Stop();
 }
