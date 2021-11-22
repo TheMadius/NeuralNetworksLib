80519d375fcbc052827bd786ae470dd2ac7a9fdc
Anna Bobr <bbfgmcr@gmail.com>
2021-11-22 22:48:21 +0700

    Доавблен метод Move (случайно)

--- Test Environment/Test Environment/Test Environment/CheckersGameAI.cpp
+++ Test Environment/Test Environment/Test Environment/CheckersGameAI.cpp
@@ -10,3 +10,36 @@ CheckersGameAI::CheckersGameAI(CheckersGame* game, double gamma, Team turnTeam)
 	this->turn = turnTeam;
 }
 
+void CheckersGameAI::Move(bool train)
+{
+	auto input = getInputVector();
+	auto legal_move = getLegalVector();
+
+	int moveId = this->qmod->explore(legal_move);
+
+	Coord coordCheck = getCoord(moveId/64);
+	Coord coordCheckMove = getCoord(moveId%64);
+
+	auto checkers = game->GetCheckers();
+
+	for (auto& i : checkers)
+	{
+		if (i->coord == coordCheck)
+		{
+			auto moves = this->game->PossibleMoves(i);
+			for (auto& move : moves)
+			{
+				if (move.target == coordCheckMove)
+				{
+					this->game->MakeMove(i, move);
+					return;
+				}
+			}
+		}
+	}
+}
+
+CheckersGameAI::~CheckersGameAI()
+{
+	delete qmod;
+}
