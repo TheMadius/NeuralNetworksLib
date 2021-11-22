96aae57075db7bf1f18fccf20bbe8a94b2a8a3de
jarviskenway <61545692+jarviskenway@users.noreply.github.com>
2021-11-22 22:50:36 +0700

    Входной вектор

--- Test Environment/Test Environment/Test Environment/CheckersGameAI.cpp
+++ Test Environment/Test Environment/Test Environment/CheckersGameAI.cpp
@@ -1,1 +1,19 @@
 #include "CheckersGameAI.h"
+
+RowVector CheckersGameAI::getInputVector()
+{
+	auto checkers = game->GetCheckers();
+	RowVector input_vextor(sol.front());
+	input_vextor.setZero();
+
+	for (auto& i : checkers)
+	{
+		int x = i->coord.x;
+		int y = i->coord.y;
+		int turn = (int)(i->team);
+
+		input_vextor.coeffRef(getIndexForArray(x , y) * COUNT_TEAM + turn) = 1.0;
+	}
+
+	return input_vextor;
+}
