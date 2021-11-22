2fb1ea3ed9607a6b895d333bdf243290e6d23c35
Anna Bobr <bbfgmcr@gmail.com>
2021-11-22 22:46:35 +0700

    Constr

--- Test Environment/Test Environment/Test Environment/CheckersGameAI.cpp
+++ Test Environment/Test Environment/Test Environment/CheckersGameAI.cpp
@@ -1,1 +1,12 @@
 #include "CheckersGameAI.h"
+
+CheckersGameAI::CheckersGameAI(CheckersGame* game, double gamma, Team turnTeam)
+{
+	this->sol = { 64 * 2, 200, 500, 2000, 4096 };
+
+	this->qmod = new QModel(sol);
+	this->game = game;
+	this->gamma = gamma;
+	this->turn = turnTeam;
+}
+
