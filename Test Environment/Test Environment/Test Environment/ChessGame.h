5bea8df5dfb8efc6f789e62defb2a6278f8824fe
Амельченко Артем Олегович <amelchenko.2018@stud.nstu.ru>
2021-11-24 22:00:48 +0700

    Added chess

---
+++ Test Environment/Test Environment/Test Environment/ChessGame.h
@@ -0,0 +1,105 @@
+#pragma once
+#include <vector>
+
+enum class FigureType
+{
+	Pawn, Knight, Bishop, Rook, Queen, King
+};
+
+enum class Team
+{
+	Black, White
+};
+
+struct Coord
+{
+	Coord(int x, int y) : x(x), y(y) {}
+	Coord operator +(const Coord& other) const { return Coord(x + other.x, y + other.y); }
+	Coord operator -(const Coord& other) const { return *this + -other; }
+	Coord operator -() const { return Coord(-x, -y); }
+	Coord operator *(int k) const { return Coord(x * k, y * k); }
+	bool operator ==(const Coord& other) const { return x == other.x && y == other.y; }
+	int x, y;
+};
+
+struct Figure
+{
+	Figure(const Coord& coord, FigureType type, Team team) : coord(coord), type(type), team(team) {}
+	bool operator ==(const Figure& other) const { return coord == other.coord && type == other.type && team == other.team; }
+	Coord coord;
+	FigureType type;
+	Team team;
+};
+
+struct Move
+{
+	Move(const Coord& figureCoord, const Coord& target) : target(target), figureCoord(figureCoord) {};
+	bool operator==(const Move& other) { return figureCoord == other.figureCoord && target == other.target; }
+	bool operator!=(const Move& other) { return !(*this == other); }
+	const Coord figureCoord;
+	Coord target;
+};
+
+struct GameInfo
+{
+	GameInfo() : isEnd(false), winner(Team::Black), turn(Team::White), allFigures(0), blackFigures(0), whiteFigures(0), countMoves(0) {}
+	bool isEnd;
+	Team winner;
+	Team turn;
+	int allFigures, blackFigures, whiteFigures;
+	int countMoves;
+};
+
+
+class ChessGame
+{
+public:
+	ChessGame();
+
+	void NewGame();
+
+	GameInfo GetInfo() const; //получить текущую информацию об игре
+
+	const Figure* GetChosenFigure() const; //получить выбранную фигуру
+
+	std::vector<const Figure*> GetFigures() const; //все фигуры на поле
+
+	std::vector<Move> PossibleMoves(const Figure* figure); //доступные клетки для хода фигуры
+
+	void Action(const Coord& coord); //нажатие на клетку
+
+	bool MakeMove(const Figure* figure, const Move& move);
+
+protected:
+
+	Figure* chosenFigure;
+
+	std::vector<Figure*> figures;
+
+	GameInfo info;
+
+	Team turnTeam;
+
+	std::vector<Move> history;
+
+private:
+
+	Figure* FigureByCoords(const Coord& coord); //получить фигуру по координатам
+
+	void InitializeGame();
+
+	bool CoordsInField(const Coord& coord);
+
+	bool CoordIsEmpty(const Coord& coord);
+
+	bool CheckEndGame();
+
+	void UpdateInfo();
+
+	void ChooseFigure(const Coord& coord);
+
+	void MakeTurn(const Move& move);
+
+	bool IsMovePossible(const Coord& from, const Coord& to) const;
+};
+
